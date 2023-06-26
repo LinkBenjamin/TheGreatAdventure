6d0463adeb8c4a04baa7b146f75440e158109170
music <music@bullwinkle.local>
2023-06-26 14:48:59 -0700

    env

--- chain.py
+++ chain.py
@@ -8,35 +8,45 @@ from langchain.prompts import PromptTemplate # import PromptTemplate
 # from langchain.memory import ChatMessageHistory
 # from langchain.memory import ConversationBufferMemory
 import prompt
+import os
 
 # history = ChatMessageHistory()
 
 
-llm = OpenAI(temperature=0.7, openai_api_key="")
+llm = OpenAI(temperature=0.7, openai_api_key=os.environ['OPENAI_API_KEY'])
 
 
 name = st.text_input(
     "Name  ",
 )
-age = st.text_input(
-        "Age: ",
-        )
-interests = st.text_input(
-        "Interests: "
-        )
-topic = st.text_input(
-        "Topic: "
-        )
+if not os.path.isfile(f'{name}.history'):
+    age = st.text_input(
+            "Age: ",
+            )
+    interests = st.text_input(
+            "Interests: "
+            )
+    topic = st.text_input(
+            "Topic: "
+            )
+    extender = ''
+else:
+    age='same as before',
+    interests = 'same as before'
+    topic = 'same as before'
+    with open(f'{name}.history', 'r') as f:
+        extender = f.read()
 
 
 
 if st.button("Tell me a story", type="primary"):
 
 
-    template = prompt.prompt1
+    template = prompt.prompt1 + extender
     prompt_template = PromptTemplate(input_variables=["name", "age", "interests", "topic"], template=template)
-    with open(f'{name}.history', 'a') as f:
-        f.write(prompt.prompt1.format(name=name, age=age, interests=interests, topic=topic))
+    if not os.path.isfile(f'{name}.history'):
+        with open(f'{name}.history', 'a') as f:
+            f.write(prompt.prompt1.format(name=name, age=age, interests=interests, topic=topic))
     question_chain = LLMChain(llm=llm, prompt=prompt_template)
 
     template = '''summarize the following in a way that can be used to prompt creation of an image::
